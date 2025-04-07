31b03fe785fbeb94296b61505f63a75df1d87455
bigmat <matinmoein511@gmail.com>
2025-04-07 12:52:52 +0330

    Resolving some problems

--- Core/Projects/api/v1/views.py
+++ Core/Projects/api/v1/views.py
@@ -1,22 +1,29 @@
-# from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from .serializer import TaskSerializer
 from ...models import Task
 from rest_framework import viewsets
 
-# class TaskListCreateAPIView(ListCreateAPIView):
-#     serializer_class = TaskSerializer
-#     queryset = Task.objects.all()
-#
-#
-# class TaskDetailUpdateDeleteView(RetrieveUpdateDestroyAPIView):
-#     serializer_class = TaskSerializer
-#     queryset = Task.objects.all()
-#     lookup_field = 'pk'
 
-
-class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+class TaskListCreateAPIView(ListCreateAPIView):
+    """
+    API endpoint that shows all tasks
+    """
     serializer_class = TaskSerializer
+    queryset = Task.objects.all()
 
+    def perform_create(self, serializer):
+        serializer.save(owner = self.request.user)
+
+
+class TaskDetailUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    lookup_field = 'pk'
+
+
+# class TaskViewSet(viewsets.ModelViewSet):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#
 

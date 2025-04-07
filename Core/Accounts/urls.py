bcf7ee95a43a8ded8deaa6348052311390494d74
bigmat <matinmoein511@gmail.com>
2025-04-07 13:37:05 +0330

    Starting API for Accounts

--- Core/Accounts/urls.py
+++ Core/Accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.urls import include
+
 from .views import LoginUserView, user_logout_view, UserRegisterView
 
+app_name = 'accounts'
+
 urlpatterns = [
-    path('login/', LoginUserView.as_view(), name='user_login'),
-    path('logout/', user_logout_view, name='user_logout'),
-    path('register/', UserRegisterView.as_view(), name='user_register'),
+    # path('login/', LoginUserView.as_view(), name='user_login'),
+    # path('logout/', user_logout_view, name='user_logout'),
+    # path('register/', UserRegisterView.as_view(), name='user_register'),
+    path('', include('django.contrib.auth.urls')),
+    path('api/v1/', include('Accounts.api.v1.urls')),
 ]
